75917123a2a6750a9a522db1fb6ebcdb782fd813
Willian Agostini <willian.agostini@gmail.com>
2022-09-07 00:46:35 -0300

    feat: salvar videos processados em arquivo

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { PromisePool } from "@supercharge/promise-pool";
+import { existsSync, readFileSync, writeFileSync } from "node:fs";
 import { findVideosBy } from "./lib/findVideos";
 import { Process } from "./lib/process";
 
@@ -12,20 +13,37 @@ async function execute() {
   //   const a = error.toJSON();
   //   console.log(error.toJSON())
   // }
-    const videoIds = [
-    "JSRtYpNRoN0",
-    "3za0Ao2fQmE",
-    "p8PZvGGe13I",
-    "y7Ulq5dvTpo",
-  ];
+  let videoIds = readFileSync('./videoIds').toString().split('\n').filter(x => x);
+  // const videoIds = [
+  //   "sjmwa6KR52U",
+  //   "nSYhwtKoD2o",
+  //   "SEpX6kgsPUw",
+  //   "-7GHgrZZi5U",
+  //   "L_Ir9FzP9QQ"
+  // ];
 
-  const { results, errors } = await PromisePool.withConcurrency(3)
+  // videoIds = await findVideosBy('gameplay farcry');
+
+  const analised = alreadyAnalised();
+  videoIds = videoIds.filter(x => !analised.includes(x))
+
+  const { results, errors } = await PromisePool.withConcurrency(4)
     .for(videoIds)
     .process(async (videoId) => {
       console.log("New Process_____________")
       return await new Process(videoId).start();
     });
   console.log(results, errors);
+  return videoIds;
 }
 
-execute();
+execute().then(x => {
+  let videoIds = existsSync('./analisados') ? alreadyAnalised() : [];
+  videoIds = videoIds.concat(x);
+  writeFileSync('./analisados', videoIds.join('\n').toString())
+
+});
+
+function alreadyAnalised() {
+  return readFileSync('./analisados').toString().split('\n').filter(x => x);
+}
